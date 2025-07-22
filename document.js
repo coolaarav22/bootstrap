6793c5558284f909350f4382923c7ac155970ca7
coolaarav22 <dkgill33@gmail.com>
2025-07-22 18:47:04 +0530

    Add files via upload

--- document.js
+++ document.js
@@ -4,3 +4,5 @@ div.className="alert";
 
 div.innerHTML="<p>congrats! you have create a div</p>";
 document.body.append(div);
+let heading=document.getElementById("heading")
+heading.innerHTML="&#128512";
