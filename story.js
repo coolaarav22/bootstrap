7e8d2086496c1c8549c5dbd9ce0a698bb2a207d3
coolaarav22 <dkgill33@gmail.com>
2025-07-24 18:54:49 +0530

    Add files via upload

---
+++ story.js
@@ -0,0 +1,12 @@
+function Typeandscream() {
+    var personname = document.getElementById("name").Value;
+    alert("I told you not to do what" + personname);
+
+}
+function godark() {
+    document.body.className = "dark";
+}
+function finishthestory() {
+    document.getElementById("story").innerHTML = "So I carefully searrched the damp dungeon there you are!";
+
+}
