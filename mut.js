645ccbdc2d124ab2816871057fb7118aba6fd4a6
coolaarav22 <dkgill33@gmail.com>
2025-07-25 16:02:27 +0530

    Add files via upload

--- mut.js
+++ mut.js
@@ -1,3 +1,14 @@
-functionmultiplication(
-    console.log (2/2)
-)
+function productoftwonumber(){
+
+var x,y,z
+x=92;
+y=23;
+z=y*x;
+
+return z;
+
+}
+
+
+var product=2116;
+console.log(`the product of 92 and 23 is `+ product)
