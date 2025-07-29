87d76cff864f2782ac03c8c794ea5930f244a07f
coolaarav22 <dkgill33@gmail.com>
2025-07-29 19:00:02 +0530

    Add files via upload

---
+++ rect.js
@@ -0,0 +1,14 @@
+let div= document.querySelector(".rectangle")
+
+function ChangesToClircle(){
+    div.style.borderRadius="50%";
+}
+
+function hide(){
+    div.style.display="none"
+}
+
+function show(){
+    div.style.display="block"
+    div.style.borderRadius="0%"
+}
