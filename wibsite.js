ee21fb7223fbacc055e1f9cbfc9bab500d37d881
coolaarav22 <dkgill33@gmail.com>
2025-08-02 19:16:45 +0530

    Add files via upload

---
+++ wibsite.js
@@ -0,0 +1,6 @@
+function lion(){
+    document.getElementById("lion-info").innerhtml="The lion (Panthera leo) is a species in the family Felidae and a member of the genus Panthera. It is known as the king of the jungle and lives in groups called prides.";
+}
+function tiger(){
+    document.getElementById("tiger-info").innerHTML="The tiger (Panthera tigris) is the largest extant cat species and a member of the genus Panthera. It is most recognizable for its dark vertical stripes on orange-brown fur with a lighter underside."
+}
