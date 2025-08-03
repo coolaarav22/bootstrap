0beb1adb3c7da25ea9e5040f71e475da31d3240c
coolaarav22 <dkgill33@gmail.com>
2025-08-03 09:58:46 +0530

    Add files via upload

--- hollies.js
+++ hollies.js
@@ -1,4 +1,12 @@
-function thisisyourhobbies(){
-buttonclick
-alert("ok this is your hobbies?")
+function Typeandscream() {
+    var personname = document.getElementById("name").Value;
+    alert("I told you not to do what" + personname);
+
+}
+function godark() {
+    document.body.className = "dark";
+}
+function finishthestory() {
+    document.getElementById("story").innerHTML = "So I carefully searrched the damp dungeon there you are!";
+
 }
